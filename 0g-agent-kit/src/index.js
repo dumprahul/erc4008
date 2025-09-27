2ca5625f1f486241463cfeca8482cd659f9077fe
Prateush Sharma <prateushsharma@gmail.com>
2025-09-28 05:05:27 +0530

    exports added

---
+++ 0g-agent-kit/src/index.js
@@ -0,0 +1,4 @@
+export { default } from './src/0gAgentSdk.js';
+export * as IdentityRegistry from './src/IdentityRegistry.js';
+export * as ReputationRegistry from './src/ReputationRegistry.js';
+export * as ValidationRegistry from './src/ValidationRegistry.js';
